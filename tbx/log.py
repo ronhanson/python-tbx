fd90a895055988f71f340b06fb130f58dafe9332
Ronan Delacroix <ronan.delacroix@gmail.com>
2018-10-05 20:17:46 +0200

    Reviewed logging, logging config and levels per handler. (v1.9.0)

--- tbx/log.py
+++ tbx/log.py
@@ -18,7 +18,7 @@ def configure_logging_to_screen(debug=False):
     if debug:
         level = 'DEBUG'
     (script_folder, app_name) = code.get_app_name()
-    settings = {'LOGGING_LEVEL': level, 'LOGGING_METHODS': ['SCREEN'], 'SCREEN_FORMAT': '%(levelname)-8s| %(message)s'}
+    settings = {'LOGGING_SCREEN_LEVEL': level, 'LOGGING_METHODS': ['SCREEN'], 'LOGGING_SCREEN_FORMAT': '%(levelname)-8s| %(message)s'}
     configure_logger(logging.getLogger(), app_name, settings=settings)
 
 
@@ -29,7 +29,7 @@ def configure_logging(log_name, settings={}, application_name=None, force=False)
 def configure_logger(logger, log_name, settings={}, application_name=None, force=False):
 
     log_level = settings.get('LOGGING_LEVEL', 'DEBUG')
-    log_methods = settings.get('LOGGING_METHODS', ['SCREEN', 'FILE', 'SYSLOG'])
+    log_methods = settings.get('LOGGING_METHODS', ['SCREEN', 'FILE'])
 
     logger.setLevel(log_level)
     
@@ -57,25 +57,31 @@ def configure_logger(logger, log_name, settings={}, application_name=None, force
 
 
 def add_screen_logging(logger, settings={}):
-    screen_format = settings.get('SCREEN_FORMAT', '%(levelname)-8s| %(message)s')
+    screen_format = settings.get('LOGGING_SCREEN_FORMAT', '%(levelname)-8s| %(message)s')
+    level = settings.get('LOGGING_SCREEN_LEVEL', None)
     write_to_screen_handler = logging.StreamHandler()
     screen_formatter = logging.Formatter(screen_format, '%Y-%m-%dT%H:%M:%S')
     write_to_screen_handler.setFormatter(screen_formatter)
+    if level:
+        write_to_screen_handler.level = level
     logger.addHandler(write_to_screen_handler)
 
 
 def add_syslog_logging(logger, log_name, settings={}):
     #guessing syslog address
     syslog_address = settings.get('LOGGING_SYSLOG_ADDRESS', None)
+    level = settings.get('LOGGING_SYSLOG_LEVEL', None)
     if not syslog_address:
         if sys.platform == 'darwin':
             syslog_address = '/var/run/syslog'
         else:
             syslog_address = '/dev/log'
-    syslog_format = settings.get('SYSLOG_FORMAT', log_name+': [%(filename)s:%(funcName)s:%(lineno)d]\t%(levelname)s - %(message).1900s')
+    syslog_format = settings.get('LOGGING_SYSLOG_FORMAT', log_name+': [%(filename)s:%(funcName)s:%(lineno)d]\t%(levelname)s - %(message).1900s')
     write_to_syslog_handler = logging.handlers.SysLogHandler(address=syslog_address)
     syslog_formatter = logging.Formatter(syslog_format, '%Y-%m-%dT%H:%M:%S')
     write_to_syslog_handler.setFormatter(syslog_formatter)
+    if level:
+        syslog_formatter.level = level
     logger.addHandler(write_to_syslog_handler)
 
 
@@ -84,7 +90,7 @@ def add_file_logging(logger, log_name, application_name, settings={}):
     if not application_name:
         application_name = app_name
 
-    log_folder = settings.get('LOGGING_FOLDER', None)
+    log_folder = settings.get('LOGGING_FILE_FOLDER', None)
     if not log_folder:
         log_folder = os.path.join(script_folder, 'log')
 
@@ -116,20 +122,24 @@ def add_file_logging(logger, log_name, application_name, settings={}):
             print("Impossible to log with FILE handler to %s either, abandoning file logging." % log_folder)
             return
 
-    file_format = settings.get('FILE_FORMAT', None)
+    file_format = settings.get('LOGGING_FILE_FORMAT', None)
 
     log_file = os.path.join(log_folder, log_name + ".txt")
 
-    add_logging_file_handler(logger, log_file, file_format)
+    level = settings.get('LOGGING_FILE_LEVEL', 'DEBUG')
+
+    add_logging_file_handler(logger, log_file, file_format, level=level)
 
 
-def add_logging_file_handler(logger, log_file, format=None):
+def add_logging_file_handler(logger, log_file, format=None, level=None):
     if not format:
         format = '[%(asctime)s] [%(filename)s:%(funcName)s:%(lineno)d]\t%(levelname)-8s - %(message)s'
 
     write_to_file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=10000000, backupCount=10, encoding='UTF-8')
     file_formatter = logging.Formatter(format, '%Y-%m-%dT%H:%M:%S')
     write_to_file_handler.setFormatter(file_formatter)
+    if level:
+        write_to_file_handler.level = level
     logger.addHandler(write_to_file_handler)
 
 
@@ -184,6 +194,9 @@ def add_mongo_logging(logger, log_name, application_name, settings={}):
     #logger.addFilter(MongoFilter())
 
     log4mongo_handler = mongo_handler_class(**mongo_handler_args)
+
+    if settings.get('LOGGING_MONGO_LEVEL', 'DEBUG'):
+        log4mongo_handler.setLevel(settings.get('LOGGING_MONGO_LEVEL'))
 
     logger.addHandler(log4mongo_handler)
 
